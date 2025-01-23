18b73e7474012f58b369111ddf72743e5ca03b7a
nakibul11426 <nakibul.hassan@bjitgroup.com>
2025-01-23 17:01:30 +0600

    [Android] Added login and reg screen bg image

--- app/src/main/java/com/alex/medify/screens/LoginScreen.kt
+++ app/src/main/java/com/alex/medify/screens/LoginScreen.kt
@@ -1,10 +1,35 @@
 package com.alex.medify.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.alex.medify.R
 
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier, navController: NavController) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.login_screen_bg),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 30.dp),
+        ){
 
+        }
+    }
 }
--- app/src/main/java/com/alex/medify/screens/RegistrationScreen.kt
+++ app/src/main/java/com/alex/medify/screens/RegistrationScreen.kt
@@ -1,10 +1,37 @@
 package com.alex.medify.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.alex.medify.R
 
 @Composable
 fun RegistrationScreen(modifier: Modifier = Modifier,navController: NavController) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.registration_screen_bg),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 30.dp),
+        ){
 
+        }
+    }
 }
